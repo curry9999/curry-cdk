a4061e98efb2c57b68d7f3e252f97c34552252c0
curry9999 <curry123456789@gmail.com>
2020-05-12 04:01:05 +0900

    initial

---
+++ bin/curry-cdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { Ec2Stack } from '../lib/ec2';
+
+/* app */
+const app = new cdk.App();
+
+/* Stacks */
+new Ec2Stack(app, 'Ec2Stack');
