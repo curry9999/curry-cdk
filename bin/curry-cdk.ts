bb505237ed44be2956af96efb2d09e8bbaa9cd41
curry9999 <curry123456789@gmail.com>
2020-05-16 01:49:55 +0900

    Split stack

--- bin/curry-cdk.ts
+++ bin/curry-cdk.ts
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import cdk = require('@aws-cdk/core');
 import { Ec2Stack } from '../lib/ec2';
+import { LabmdaStack } from '../lib/lambda';
 
 /* app */
 const app = new cdk.App();
 
-/* Stack */
+/* Stack EC2 */
 const ec2stack = new Ec2Stack(app, 'Ec2Stack');
 ec2stack.tags.setTag('autostop','1');
+
+/* Stack Labmda */
+const lambdastack = new LabmdaStack(app, 'LabmdaStack');
