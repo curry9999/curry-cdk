a823d3e3c7e0bae99c8dfd1cdc3b93ebc243f188
curry9999 <curry123456789@gmail.com>
2020-05-30 18:56:26 +0900

    Change profile name

--- cdk-deploy-prod.sh
+++ cdk-deploy-prod.sh
@@ -2,8 +2,8 @@
 # cdk-deploy-prod.sh
 
 # default account and region
-export CDK_DEFAULT_ACCOUNT=`aws sts get-caller-identity --profile test --query "Account" --output text`
-export CDK_DEFAULT_REGION=`aws configure get region --profile test`
+export CDK_DEFAULT_ACCOUNT=`aws sts get-caller-identity --profile prod --query "Account" --output text`
+export CDK_DEFAULT_REGION=`aws configure get region --profile prod`
 
 # execute cdk-deploy-core.sh
 bash cdk-deploy-core.sh ${CDK_DEFAULT_ACCOUNT} ap-northeast-1 "$@"
