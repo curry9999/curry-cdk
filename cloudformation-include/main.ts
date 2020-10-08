4634d38438d88f72c69835ed4e996eb64740cf89
curry9999 <curry123456789@gmail.com>
2020-10-09 01:11:54 +0900

    Add cloudformation-include sample

---
+++ cloudformation-include/main.ts
@@ -0,0 +1,18 @@
+import * as cdk from '@aws-cdk/core';
+import * as cfn_inc from '@aws-cdk/cloudformation-include';
+import 'source-map-support/register';
+
+export class MigrationStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const cfnInclude = new cfn_inc.CfnInclude(this, 'Template', { 
+      templateFile: 'VpcStack.yml',
+    });
+  }
+}
+
+const app = new cdk.App();
+//'MigrationStack' below needs to be the same name as the CloudFormation name
+new MigrationStack(app, 'VpcStack');
