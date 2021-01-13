c01a7b5cb32cbfdac12c35bf1a44023e716e9d9d
curry9999 <curry123456789@gmail.com>
2021-01-14 02:05:56 +0900

    add s3 stacks

---
+++ s3/cdk-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# cdk-deploy.sh
+
+# env
+## input args
+export AWS_PROFILE=$1
+export STACK_NAME=$2
+
+# main
+## check cdk stack count
+test `cdk ls --require-approval never | wc -l` != 1 && test $# != 2 && echo "*** ERROR: Please specify the stack name in the argument" && cdk ls --require-approval never && exit 1
+
+# execute cdk synth
+cdk synth --require-approval never ${STACK_NAME}
+# execute cdk deploy
+cdk deploy --require-approval never ${STACK_NAME}
